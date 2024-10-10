a1e633c3f7538734f1171fc270ecceaa47b9e5a3
wolbert <wolbert@bluewin.ch>
2024-10-10 13:49:22 +0200

    shithead: add reverting of statistics.
    During a game round score, turn count, and game count is updated in the
    statistics whenever a player goes out. In case of an aborted game round
    we have to revert the statistics to the previous values.
    There's now also the possibility that we have to print a result table
    with 0 games, i.e. we have to avoid division through zero.

--- src/shithead/stats.py
+++ src/shithead/stats.py
@@ -41,6 +41,8 @@ class Statistics():
         '''
         # table returns all-0 if key is not in dictionary
         self.table = defaultdict(self.default_val)
+        # name of last Shithead => dealer of next game
+        self.shithead = None
 
     def update(self, name, score, turns):
         '''
@@ -62,6 +64,7 @@ class Statistics():
         # a score of 0 means the player was shithead
         if score == 0:
             self.table[name][SH_COUNT] += 1 # increment the shit count
+            self.shithead = name
 
     def set_stats(self, name, counters):
         '''
@@ -79,6 +82,32 @@ class Statistics():
         self.table[name][SCORE] = counters[SCORE]
         self.table[name][GAMES] = counters[GAMES]
         self.table[name][TURNS] = counters[TURNS]
+
+    def revert(self, name, score, turns):
+        '''
+        Revert player's score, turn count, and game count.
+
+        If a round of the game has to be aborted, because to many turns have
+        been played (AI deadlock) the statistics for this round have to be
+        annullated. Since statistics are updated whenver a player went out, we
+        have to revert the statistics of these players.
+        NOTE: there's no need for reverting the shithead counter, since there's
+              no shithead in an aborted game.
+
+        :param name:    name of player.
+        :type name:     str
+        :param score:   number of players still in the game when this player went out,
+                        e.g. for  3 players: 1st => 2, 2nd => 1, 3rd (= shithead) => 0.
+        :type score:    int
+        :param turns:   number of turns played this game.
+        :type turns:    int
+        '''
+        self.table[name][SCORE] -= score   # total score
+        self.table[name][TURNS] -= turns   # total number of turns
+        self.table[name][GAMES] -= 1       # number of games played
+
+
+
 
     def get_stats(self, name):
         '''
@@ -102,12 +131,16 @@ class Statistics():
         total_score = 0
         total_games = 0
         total_turns = 0
+        total_avg_turns = 0
         # sort the list of table items (= key,value tuples)
         # by value (x[1]) in normal order.
         sorted_table = sorted(self.table.items(), key=lambda x:x[1][SH_COUNT], reverse=False)
         for name,value in sorted_table:
-            sh_percent = value[SH_COUNT] / value[GAMES] * 100
-            avg_turns = value[TURNS] / value[GAMES]
+            sh_percent = 0
+            avg_turns = 0
+            if value[GAMES] > 0:
+                sh_percent = value[SH_COUNT] / value[GAMES] * 100
+                avg_turns = value[TURNS] / value[GAMES]
             entry = [name, f'{value[SH_COUNT]}', f'{sh_percent:.1f}%',
                     f'{value[SCORE]}', f'{value[GAMES]}', f'{value[TURNS]}', f'{avg_turns:.1f}']
             table.append(entry)
@@ -116,7 +149,8 @@ class Statistics():
             total_score += value[SCORE]
             total_games = value[GAMES]  # don't sum up
             total_turns += value[TURNS]
-        total_avg_turns = total_turns / total_games
+        if total_games > 0:
+            total_avg_turns = total_turns / total_games
         # add entry with totals
         entry = [f'{len(sorted_table)}', f'{total_sh_count}',
                 f'{total_sh_percent:.1f}%', f'{total_score}', f'{total_games}',
@@ -172,20 +206,25 @@ class Statistics():
         total_turns = 0
         total_sh_count = 0
         total_sh_percent = 0
+        total_avg_turns = 0
         #print sorted table
         print('+--------------------+--------------------+----------+----------+----------+------------+')
         print('| Player             |       Shithead     |    Score |    Games |    Turns | Turns/Game |')
         print('+--------------------+-----------+--------+----------+----------+----------+------------+')
         for name,value in sorted_table:
-            sh_percent = value[SH_COUNT] / value[GAMES] * 100
-            avg_turns = value[TURNS] / value[GAMES]
+            sh_percent = 0
+            avg_turns = 0
+            if value[GAMES] > 0:
+                sh_percent = value[SH_COUNT] / value[GAMES] * 100
+                avg_turns = value[TURNS] / value[GAMES]
             print(f'| {name:<19}|{value[SH_COUNT]:>10} |{sh_percent:>6.1f}% |{value[SCORE]:>9} |{value[GAMES]:>9} |{value[TURNS]:>9} |{avg_turns:11.2f} |')
             total_games = value[GAMES] # don't sum up
             total_score += value[SCORE]
             total_turns += value[TURNS]
             total_sh_count += value[SH_COUNT]
             total_sh_percent += sh_percent
-        total_avg_turns = total_turns / total_games
+        if total_games > 0:
+            total_avg_turns = total_turns / total_games
         print('+--------------------+-----------+--------+----------+----------+----------+------------+')
         print(f"| {len(sorted_table):<19}|{total_sh_count:>10} |{total_sh_percent:>6.1f}% |{total_score:>9} |{total_games:>9} |{total_turns:>9} |{total_avg_turns:>11.2f} |")
         print('+--------------------+-----------+--------+----------+----------+----------+------------+')
@@ -206,21 +245,26 @@ class Statistics():
         total_turns = 0
         total_sh_count = 0
         total_sh_percent = 0
+        total_avg_turns = 0
         # write sorted table to file
         with open(filename, 'w') as f:
             f.write('+--------------------+--------------------+----------+----------+----------+------------+\n')
             f.write('| Player             |       Shithead     |    Score |    Games |    Turns | Turns/Game |\n')
             f.write('+--------------------+-----------+--------+----------+----------+----------+------------+\n')
             for name,value in sorted_table:
-                sh_percent = value[SH_COUNT] / value[GAMES] * 100
-                avg_turns = value[TURNS] / value[GAMES]
+                sh_percent = 0
+                avg_turns = 0
+                if value[GAMES] > 0:
+                    sh_percent = value[SH_COUNT] / value[GAMES] * 100
+                    avg_turns = value[TURNS] / value[GAMES]
                 f.write(f'| {name:<19}|{value[SH_COUNT]:>10} |{sh_percent:>6.1f}% |{value[SCORE]:>9} |{value[GAMES]:>9} |{value[TURNS]:>9} |{avg_turns:11.2f} |\n')
                 total_games = value[GAMES]  # don't sum up
                 total_score += value[SCORE]
                 total_turns += value[TURNS]
                 total_sh_count += value[SH_COUNT]
                 total_sh_percent += sh_percent
-            total_avg_turns = total_turns / total_games
+            if total_games > 0:
+                total_avg_turns = total_turns / total_games
             f.write('+--------------------+-----------+--------+----------+----------+----------+------------+\n')
             f.write(f"| {len(sorted_table):<19}|{total_sh_count:>10} |{total_sh_percent:>6.1f}% |{total_score:>9} |{total_games:>9} |{total_turns:>9} |{total_avg_turns:>11.2f} |\n")
             f.write('+--------------------+-----------+--------+----------+----------+----------+------------+\n')
